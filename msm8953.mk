9726bf5ce12a73af211df4f5dd8ec4fbbe2328ce
DevilGhoul <abdul.4pda05@yandex.com>
2018-10-16 15:52:27 +0000

    AOSIP

--- msm8953.mk
+++ msm8953.mk
@@ -387,9 +387,9 @@ PRODUCT_PACKAGES += \
     wpa_supplicant.conf
 
 # XiaomiParts
-PRODUCT_PACKAGES += \
-    XiaomiParts \
-    Messaging
+#PRODUCT_PACKAGES += \
+   # XiaomiParts \
+   # Messaging
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
